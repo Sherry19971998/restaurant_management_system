dc731158a784084fcc8c5d0ffd6b7cd4774144a1
anh-nguyen9-student <anguyen9@arizona.edu>
2026-04-27 13:33:20 -0700

    Update updateTableStatus API endpoint to include '/status'

--- frontend/src/api/table.js
+++ frontend/src/api/table.js
@@ -5,4 +5,5 @@ const API_BASE = '/api/tables';
 export const getTables = () => axios.get(API_BASE);
 export const getTable = (id) => axios.get(`${API_BASE}/${id}`);
 export const addTable = (data) => axios.post(API_BASE, data);
-export const updateTableStatus = (id, data) => axios.patch(`${API_BASE}/${id}`, data);
+export const updateTableStatus = (id, data) => axios.patch(`${API_BASE}/${id}/status`, data);
+  
